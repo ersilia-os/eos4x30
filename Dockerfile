db53cc63980456cec4d84aab1e024472d85c7d2d
GemmaTuron <gemma@ersilia.io>
2023-11-28 14:15:28 +0100

    add model

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
-FROM bentoml/model-server:0.11.0-py37
+FROM bentoml/model-server:0.11.0-py310
 MAINTAINER ersilia
 
-RUN pip install rdkit
+RUN pip install rdkit==2023.9.2
+RUN pip install datamol==0.12.1
+RUN pip install molfeat==0.9.5
 
 WORKDIR /repo
 COPY . /repo
--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -2,8 +2,11 @@
 import os
 import csv
 import sys
-from rdkit import Chem
-from rdkit.Chem.Descriptors import MolWt
+import datamol as dm
+import molfeat
+from molfeat.trans.base import MoleculeTransformer
+from molfeat.calc.pharmacophore import Pharmacophore3D
+
 
 # parse arguments
 input_file = sys.argv[1]
@@ -13,8 +16,12 @@ output_file = sys.argv[2]
 root = os.path.dirname(os.path.abspath(__file__))
 
 # my model
-def my_model(smiles_list):
-    return [MolWt(Chem.MolFromSmiles(smi)) for smi in smiles_list]
+def my_model(smiles):
+    mols = [dm.to_mol(s) for s in smiles]
+    molecules_with_3D = [dm.conformers.generate(m, n_confs=1, minimize_energy=True) for m in mols]
+    transformer = MoleculeTransformer(featurizer=Pharmacophore3D(factory='pmapper'), dtype=float)    
+    features = transformer(molecules_with_3D) 
+    return features
 
 
 # read SMILES from .csv file, assuming one column with header
@@ -34,6 +41,6 @@ assert input_len == output_len
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["value"])  # header
+    writer.writerow(["pmapper-{}".format(i) for i in range(outputs.shape[1])])  # header
     for o in outputs:
-        writer.writerow([o])
+        writer.writerow(o)
