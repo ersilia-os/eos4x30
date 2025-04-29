39d9ced85b316223f46dbcb2c3baec01e081b73b
GemmaTuron <gemma@ersilia.io>
2025-04-29 13:53:12 +0200

    update workflows

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -41,6 +41,6 @@ assert input_len == output_len
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["dimension_{0}".format(str(i).zfill(4)) for i in range(outputs.shape[1])])  # header
+    writer.writerow(["dim_{0}".format(str(i).zfill(4)) for i in range(outputs.shape[1])])  # header
     for o in outputs:
         writer.writerow(o)
